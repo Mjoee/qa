ac67ccd32c540dfc43abe894275f2d81dab95c7e
Nikita Piatiletov <pyatiletovnick@gmail.com>
2020-11-06 21:01:46 +0200

    opa

--- src/test/java/lesson9/PoTest.java
+++ src/test/java/lesson9/PoTest.java
@@ -1,23 +1,23 @@
 package test.java.lesson9;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import test.java.TestBaseSetUp;
+import test.java.TestBaseSetup;
 import test.java.pages.GiftPage;
 import test.java.pages.HomePage;
 
+import java.util.List;
 
-public class PoTest extends TestBaseSetUp {
+import static org.testng.Assert.assertTrue;
 
+public class PoTest extends TestBaseSetup {
     HomePage homePage;
     GiftPage giftPage;
-
 
     @BeforeMethod
     public void pageInitialize() {
@@ -26,15 +26,24 @@ public class PoTest extends TestBaseSetUp {
     }
 
     @Test
-    public void test1(){
-
+    public void test1() {
         homePage
                 .open()
                 .clickSellBtn()
                 .clickRegistryBtn()
+                .clickSellBtn()
                 .clickGiftCardsBtn();
-        giftPage.clickPrice("$200 & Above");
+        List<WebElement> cartsList =
+                giftPage
+                        .clickPrice("$200 & Above")
+                        .getCartsList();
 
-
+        for (WebElement cart: cartsList) {
+            int actualMaximumPrice = giftPage.getMaximumCartPrice(cart);
+            assertTrue(
+                    actualMaximumPrice >= 200,
+                    String.format("Expected price %d to be more than 200", actualMaximumPrice)
+            );
+        }
     }
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,56 +1,63 @@
 package test.java.pages;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HomePage {
-    By sellBtnBy = By.xpath("(//div[@id='nav-xshop']/a)[5]");
-    By registryBtnBy = By.xpath("(//div[@id='nav-xshop']/a)[4]");
-    By giftCardsBtnBy = By.xpath("(//div[@id='nav-xshop']/a)[3]");
-
+public class HomePage extends BasePage {
+    private Logger logger = LogManager.getLogger(HomePage.class);
     WebDriver driver;
     WebDriverWait wait;
+    By sellBtnBy = By.xpath("//a[text() ='Sell']");
+    By registryBtnBy = By.xpath("//a[text() ='Registry']");
+    By giftCardsBtnBy = By.xpath("//a[text() ='Gift Cards']");
 
     public HomePage(WebDriver driver) {
+        logger.trace("HOME PAGE was initialized");
         this.driver = driver;
+        wait = new WebDriverWait(driver, 10, 500);
     }
 
     public HomePage open() {
+        logger.info("Open home page");
+        logger.warn("WARNING !!!!!");
+        logger.error("ERROR !!!!!");
+        logger.fatal("ERROR !!!!!");
         driver.get("https://www.amazon.com/");
-        wait = new WebDriverWait(driver, 10,500);
-        wait.until(new ExpectedCondition<Boolean>() {
+        wait.until(d -> d.findElements(By.xpath("//div[@id='nav-xshop']/a")).size() == 6);
+        /*wait.until(new ExpectedCondition<Boolean>() {
             @Override
-            public Boolean apply (WebDriver driver){
+            public Boolean apply( WebDriver driver) {
                 return driver.findElements(By.xpath("//div[@id='nav-xshop']/a")).size() == 6;
             }
-        });
+        });*/
         return this;
     }
 
     public HomePage clickSellBtn() {
+        logger.info("Click 'Sell' button");
         driver.findElement(sellBtnBy).click();
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("(//*[contains(text(), 'Sell on Amazon')])[1]")));
         return this;
     }
 
     public HomePage clickRegistryBtn() {
+        logger.info("Click 'Registry' button");
         driver.findElement(registryBtnBy).click();
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("(//*[@alt='Register with Amazon']")));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@alt='Register with Amazon']")));
         return this;
     }
 
-
     public HomePage clickGiftCardsBtn() {
+        logger.info("Click 'Gift cards' button");
         driver.findElement(giftCardsBtnBy).click();
         wait.until(ExpectedConditions.and(
                 ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//h1[text() = 'Gift Cards']")),
-                driver1 -> driver1.findElements(By.xpath("//div[@class='bxc-grid__image   bxc-grid__image--light']")).size() >= 3
+                d -> d.findElements(By.xpath("//div[@class='bxc-grid__image   bxc-grid__image--light']")).size() >= 3
         ));
         return this;
     }
-
-
 }
